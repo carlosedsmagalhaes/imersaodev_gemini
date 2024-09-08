bb07842367c0c2da385f64eaef2850f27a007a63
Carlos Magalhães <65139043+carlosedsmagalhaes@users.noreply.github.com>
2024-09-08 00:41:54 -0300

    Implementa funcionalidade de busca

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log(dados[0].titulo);
+function pesquisar() {
+  // Seleciona a seção onde os resultados da pesquisa serão exibidos
+  let section = document.querySelector("#resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  if (!campoPesquisa.trim()) {
+    section.innerHTML =
+      '<p class="resultado-pesquisa-vazia">Nada foi encontrado. você não digitou nenhuma informação para que a busca fosse realizada.</p>';
+    return;
+  }
+  // Inicializa uma string vazia para armazenar os resultados da pesquisa
+  let resultados = "";
+
+  // Seleciona o elemento de entrada da pesquisa
+
+  // Itera sobre os dados da pesquisa e cria o HTML para cada resultado
+  dados.forEach((dado) => {
+    if (
+      dado.titulo.toUpperCase().includes(campoPesquisa.toUpperCase()) ||
+      dado.descricao.toUpperCase().includes(campoPesquisa.toUpperCase()) ||
+      dado.palavraChave.toUpperCase().includes(campoPesquisa.toUpperCase())
+    ) {
+      resultados += `
+        <div class="item-resultado">
+          <h2>${dado.titulo}</h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>
+      `;
+    }
+  });
+
+  if (!resultados) {
+    resultados = '<p class="resultado-pesquisa-vazia">Nada foi encontrado.</p>';
+  }
+
+  // Atualiza o conteúdo da seção com os resultados da pesquisa
+  section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -1,2 +1,65 @@
-let dados = [{titulo: 'JavaScript',descricao: 'JavaScript é uma linguagem de programação indispensável para criar páginas web dinâmicas e interativas. Ela permite que os sites respondam às ações do usuário em tempo real, tornando a experiência de navegação mais fluida e intuitiva. Com JavaScript, é possível criar animações, validar formulários, manipular o conteúdo de uma página, criar jogos simples e muito mais. Além disso, frameworks como React, Angular e Vue.js, baseados em JavaScript, permitem o desenvolvimento de aplicações web complexas e de alta performance. A versatilidade do JavaScript o torna uma ferramenta essencial para qualquer desenvolvedor front-end.',link: 'https://pt.wikipedia.org/wiki/JavaScript'}, {titulo: 'Python', descricao: 'Python é uma linguagem de programação de alto nível, conhecida por sua sintaxe clara e legível, o que a torna uma excelente escolha para iniciantes e desenvolvedores experientes. Sua versatilidade é notável, permitindo a criação de uma ampla gama de aplicações, desde scripts simples para automatizar tarefas até complexos sistemas de aprendizado de máquina e inteligência artificial. Python é amplamente utilizada em diversas áreas, como desenvolvimento web, ciência de dados, análise de dados, desenvolvimento de jogos e muito mais. Sua comunidade ativa e extensa biblioteca padrão contribuem para sua popularidade e facilitam o desenvolvimento de projetos robustos e eficientes.',link: 'https://pt.wikipedia.org/wiki/Python'}, {titulo: 'C#',descricao: 'C# é uma linguagem de programação poderosa e versátil, amplamente utilizada para desenvolver uma variedade de aplicações. Com sua sintaxe clara e orientada a objetos, o C# é ideal para criar softwares robustos e escaláveis. Seja no desenvolvimento de aplicações desktop, web, jogos ou aplicativos móveis com Xamarin, o C# oferece um ecossistema rico e ferramentas poderosas. Sua integração com a plataforma .NET Framework e, mais recentemente, com o .NET Core, proporciona aos desenvolvedores uma base sólida para construir soluções modernas e eficientes. A tipagem estática e a forte verificação de tipos garantem a segurança e a confiabilidade do código, tornando o C# uma excelente escolha para projetos de grande porte.',link: 'https://pt.wikipedia.org/wiki/C_Sharp'}
+let dados = [
+  {
+    titulo: "JavaScript",
+    descricao:
+      "JavaScript é uma linguagem de programação indispensável para criar páginas web dinâmicas e interativas. Ela permite que os sites respondam às ações do usuário em tempo real, tornando a experiência de navegação mais fluida e intuitiva. Com JavaScript, é possível criar animações, validar formulários, manipular o conteúdo de uma página, criar jogos simples e muito mais. Além disso, frameworks como React, Angular e Vue.js, baseados em JavaScript, permitem o desenvolvimento de aplicações web complexas e de alta performance. A versatilidade do JavaScript o torna uma ferramenta essencial para qualquer desenvolvedor front-end.",
+    link: "https://pt.wikipedia.org/wiki/JavaScript",
+    palavraChave: "front-end, programação web, JavaScript framework, UI/UX, SPA, Node.js, React, Angular, Vue.js"
+  },
+  {
+    titulo: "Python",
+    descricao:
+      "Python é uma linguagem de programação de alto nível, conhecida por sua sintaxe clara e legível, o que a torna uma excelente escolha para iniciantes e desenvolvedores experientes. Sua versatilidade é notável, permitindo a criação de uma ampla gama de aplicações, desde scripts simples para automatizar tarefas até complexos sistemas de aprendizado de máquina e inteligência artificial. Python é amplamente utilizada em diversas áreas, como desenvolvimento web, ciência de dados, análise de dados, desenvolvimento de jogos e muito mais. Sua comunidade ativa e extensa biblioteca padrão contribuem para sua popularidade e facilitam o desenvolvimento de projetos robustos e eficientes.",
+    link: "https://pt.wikipedia.org/wiki/Python",
+    palavraChave: "back-end, data science, machine learning, deep learning, Django, Flask, AI, ciência de dados, análise de dados"
+  },
+  {
+    titulo: "C#",
+    descricao:
+      "C# é uma linguagem de programação poderosa e versátil, amplamente utilizada para desenvolver uma variedade de aplicações. Com sua sintaxe clara e orientada a objetos, o C# é ideal para criar softwares robustos e escaláveis. Seja no desenvolvimento de aplicações desktop, web, jogos ou aplicativos móveis com Xamarin, o C# oferece um ecossistema rico e ferramentas poderosas. Sua integração com a plataforma .NET Framework e, mais recentemente, com o .NET Core, proporciona aos desenvolvedores uma base sólida para construir soluções modernas e eficientes. A tipagem estática e a forte verificação de tipos garantem a segurança e a confiabilidade do código, tornando o C# uma excelente escolha para projetos de grande porte.",
+    link: "https://pt.wikipedia.org/wiki/C_Sharp",
+    palavraChave: "C#, .NET, .NET Core, desenvolvimento desktop, desenvolvimento web, Xamarin, Unity, programação orientada a objetos, framework, biblioteca"
+  },
+  {
+    titulo: "Java",
+    descricao:
+      "Java é uma linguagem orientada a objetos robusta e escalável, utilizada em uma ampla gama de aplicações, desde sistemas empresariais até aplicativos móveis (Android). Sua portabilidade (plataforma independente) e grande ecossistema de ferramentas e bibliotecas a tornam uma escolha popular para o desenvolvimento de software de grande porte.",
+    link: "https://www.oracle.com/java/",
+    palavraChave: "Java, JVM, programação orientada a objetos, desenvolvimento enterprise, Android, Spring, Hibernate, servidor de aplicação, plataforma independente"
+  },
+  {
+    titulo: "Node.js",
+    descricao:
+      "Node.js é um ambiente de execução JavaScript que permite aos desenvolvedores criar aplicações de servidor, ferramentas de linha de comando e scripts, tudo utilizando a linguagem JavaScript. Em outras palavras, o Node.js leva o JavaScript para além do navegador, permitindo que você execute código JavaScript diretamente no seu servidor.",
+    link: "https://nodejs.org/",
+    palavraChave: "Node.js, JavaScript, back-end, full-stack, real-time, servidor, API, framework, Express, NestJS"
+  },
+  {
+    titulo: "SQL",
+    descricao:
+      "SQL (Structured Query Language, ou Linguagem de Consulta Estruturada em português) é a linguagem padrão utilizada para gerenciar e manipular dados em bancos de dados relacionais.",
+    link: "https://www.w3schools.com/sql/",
+    palavraChave: "SQL, banco de dados, relacional, consulta, manipulação de dados, MySQL, PostgreSQL, SQL Server, join, aggregate functions"
+  },
+  {
+    titulo: "MongoDB",
+    descricao:
+      "Um banco de dados NoSQL document-oriented, altamente escalável e flexível. É ideal para aplicações que exigem alta disponibilidade e armazenamento de dados não estruturados.",
+    link: "https://www.mongodb.com/",
+    palavraChave: "MongoDB, NoSQL, document-oriented, banco de dados, escalabilidade, flexibilidade, JSON, schema-less, Big Data"
+  },
+  {
+    titulo: "React Native",
+    descricao:
+      "Um framework para construir aplicativos móveis nativos usando JavaScript e React. Permite criar aplicativos para iOS e Android a partir de uma única base de código.",
+    link: "https://reactnative.dev/",
+    palavraChave: "React Native, JavaScript, React, mobile, cross-platform, iOS, Android, desenvolvimento híbrido, UI"
+  },
+  {
+    titulo: "Kubernetes",
+    descricao:
+      "Uma plataforma de orquestração de contêineres open-source, amplamente utilizada para gerenciar grandes clusters de contêineres Docker. Kubernetes automatiza a implantação, a escalabilidade e a gestão de aplicações containerizadas.",
+    link: "https://kubernetes.io/",
+    palavraChave: "Rust, sistemas, segurança, desempenho, concorrência, programação sistêmica, WebAssembly"
+  }
 ];
